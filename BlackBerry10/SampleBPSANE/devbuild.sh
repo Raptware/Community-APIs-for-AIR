2100ccec09eb8c9866172d747a83ac72d5707493
julian <dolce.julian@gmail.com>
2012-07-25 09:31:14 -0400

    Adding QXN-x86 platform for the simulator, updated using the BlackBerry 10 Beta 2 NDK.

--- BlackBerry10/SampleBPSANE/devbuild.sh
+++ BlackBerry10/SampleBPSANE/devbuild.sh
@@ -3,8 +3,10 @@ AIR_SDK_PATH="/Developer/SDKs/air_3.0_sdk/bin"
 
 unzip -o as/bin/bpsane.swc
 rm catalog.xml
-cp native/Device-Debug/libbpsane-arm.so ./native.so
-eval $AIR_SDK_PATH/adt -package -target ane bpsdemo.ane extension.xml -swc as/bin/bpsane.swc -platform QNX-ARM -C . library.swf native.so 
-rm native.so
+cp native/Device-Debug/libbpsane-arm.so ./nativearm.so
+cp native/Simulator-Debug/libbpsane-x86.so ./nativex86.so
+eval $AIR_SDK_PATH/adt -package -target ane bpsdemo.ane extension.xml -swc as/bin/bpsane.swc -platform QNX-ARM -C . library.swf nativearm.so -platform QNX-x86 -C . library.swf nativex86.so 
+rm nativearm.so
+rm nativex86.so
 rm library.swf
 cp bpsdemo.ane app/bpsdemo.ane
